c864379c4d5bf89a031ca0c695ef476ffb322a13
Zakaria <zakaria7262@gmail.com>
2022-04-04 14:03:52 +0600

    add page title in reviews pages

--- src/components/Pages/Review/Review.js
+++ src/components/Pages/Review/Review.js
@@ -7,6 +7,9 @@ const Review = () => {
   const [reviews, setReviews] = useReviews();  
   return (
     <div className='w-full max-w-screen-xl mt-10 mb-40'>
+      <h3 className='bg-teal-400 p-2 rounded-t-lg border-b-2 border-orange-500 text-xl font-bold mb-4'>
+        Movie Reviews <span className='text-orange-700'>({reviews.length})</span>
+      </h3>
       <div className='grid grid-cols-3 justify-items-center items-center'>
       {
         reviews.map(review => {
