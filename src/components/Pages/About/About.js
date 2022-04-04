ce965e98e3b7ef77c03cd5aeef8ba658118bcfcd
Zakaria <zakaria7262@gmail.com>
2022-04-04 20:02:51 +0600

    about page content added

--- src/components/Pages/About/About.js
+++ src/components/Pages/About/About.js
@@ -2,9 +2,18 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <p>About</p>
+    <div className='w-full max-w-screen-xl mt-10 mb-40 flex flex-col items-center'>
+      <h1 className='text-4xl font-bold underline'>Contact</h1>
+      <button 
+        className='bg-orange-600 px-4 py-2 rounded-full text-white hover:text-black hover:bg-orange-500 duration-100 ease-in-out flex justify-center mt-5'
+        onClick={() => window.location.assign('mailto:someone@example.com')}>
+          <span className='text-xl'>
+            Mail Us
+          </span>
+      </button>
     </div>
+
+
   );
 };
 
