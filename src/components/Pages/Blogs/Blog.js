f6471d2eb7bbcd5375298c295c5514c1f62b5449
Zakaria <zakaria7262@gmail.com>
2022-04-04 17:07:14 +0600

    in blogs page blog added

---
+++ src/components/Pages/Blogs/Blog.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar, faCaretUp, faCaretDown } from '@fortawesome/free-solid-svg-icons'
+
+const Blog = ({data}) => {
+  const {question, answer, post_data} = data;
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="w-2/3 bg-orange-100 mb-2 relative">
+          <div className="flex justify-between items-center bg-orange-300 px-2 py-2">
+            <h4 className="font-semibold text-2xl">
+              {question}
+            </h4>
+            <span onClick={() => setOpen(!open)} className="hover:cursor-pointer">
+             {open ? <FontAwesomeIcon icon={faCaretUp} /> : <FontAwesomeIcon icon={faCaretDown} />}
+            </span>
+          </div>
+
+          <div className={`${!open ? 'hidden' : 'initial'}`}>
+            <p className="py-2 px-3 font-light text-lg">{answer}</p>
+            <p className="bg-orange-200 border-l-2  border-red-500 pl-2 font-medium text-red-900">
+              Post date: <span className="text-teal-900">{post_data}</span>
+            </p>
+          </div>
+        </div>
+  );
+};
+
+export default Blog;
--- src/components/Pages/Blogs/Blogs.js
+++ src/components/Pages/Blogs/Blogs.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import Blog from "./Blog";
 
 const Blogs = () => {
+  const [qna, setQna] = useState();
+
+  useEffect (() => {
+    fetch('qna.json')
+      .then (res => res.json())
+      .then (data => setQna(data))
+      .catch (() => console.log('Data Not Found'));
+  }, [])
+
   return (
-    <div>
-      <p>Blogs</p>
+    <div className="w-full max-w-screen-xl mt-10 mb-40">
+      <h3 
+        className="bg-teal-400 p-2 rounded-t-lg border-b-2 border-orange-500 text-xl font-bold mb-4">
+        Blogs 
+      </h3>
+      <div className="flex flex-col items-center">
+        {
+          qna?.map( qnaItem => {
+            return (
+              <Blog
+                key={qnaItem._id}
+                data={qnaItem}
+              ></Blog>
+            );
+          })  
+        }
+      </div>
     </div>
   );
 };
